990d32dc429f5a6414a16f93e51266fb36d66fae
AWARDS.CO.ZA\ErnstC <ernstc@awards.co.za>
2018-06-07 15:14:23 +0200

    Added Routing

--- js/app.js
+++ js/app.js
@@ -2,4 +2,26 @@
  *  Created by Ernst 05/06/2018 
  */
 
- var app = angular.module('tutorialApp', ["tutorialCtrlModule"]);
+ var app = angular.module('tutorialApp', ["ngRoute", "tutorialCtrlModule"]);
+
+ app.config(function($routeProvider){
+    $routeProvider
+
+    .when("/", {
+        templateUrl: "views/tutorial.html",
+        controller: "TutorialCtrl"
+    })
+
+    .when("/tutorialSecond", {
+        templateUrl: "views/tutorialSecond.html",
+        controller: "TutorialCtrl2"
+    })
+
+    .when("/error1", {
+        templateUrl: "views/error.html"
+    })
+
+    .otherwise({
+        redirectTo: "/error1"
+    })
+ });
